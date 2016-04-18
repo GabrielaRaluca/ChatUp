51c92dccfa81cda2ed0472df2829da318dfb2eb4
mihaimohora <mihaimohora@gmail.com>
2016-04-18 21:10:02 +0300

    Client

---
+++ ClientMDS/src/client/Client.java
@@ -0,0 +1,78 @@
+package client;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.SwingUtilities;
+
+import GUI.*;
+
+public class Client
+{
+	public Socket clientSocket;
+	public ObjectOutputStream output;
+	public ObjectInputStream input;
+	public static boolean closed = false;
+	
+	public static Conversation convo;
+	public String serverIP;
+	public int portNumber;
+	public static String message;
+
+	public Client(String IP, int port)
+	{
+		this.serverIP = IP;
+		this.portNumber = port;
+		try
+		{
+			clientSocket = new Socket(serverIP, portNumber);
+		}
+		catch(IOException ioe)
+		{
+			ioe.printStackTrace();
+		}
+	}
+	
+	public void close()
+	{
+		try
+		{
+			output.close();
+			input.close();
+			clientSocket.close();
+		}
+		catch(IOException ioe)
+		{
+			ioe.printStackTrace();
+		}
+	}
+	
+	public void showMessage(final String m)
+	{
+		SwingUtilities.invokeLater(
+				new Runnable()
+				{
+					public void run()
+					{
+						convo.messages.append(m);
+					}
+				}
+		);
+	}
+
+	public void setUpStreams()
+	{
+		try
+		{
+			output = new ObjectOutputStream(clientSocket.getOutputStream());
+			output.flush();
+			input = new ObjectInputStream(clientSocket.getInputStream());
+		}
+		catch(IOException ioe)
+		{
+			ioe.printStackTrace();
+		}
+	}
+}
